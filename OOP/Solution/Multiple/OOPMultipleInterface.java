55297f5e6be801c8a3ec4454aafd93ab6da3fb53
aviadshiber <aviadshiber@campus.technion.ac.il>
2017-05-29 21:36:19 +0300

    init project

---
+++ OOP/Solution/Multiple/OOPMultipleInterface.java
@@ -0,0 +1,14 @@
+package OOP.Solution.Multiple;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by ashiber on 29-May-17.
+ */
+@Documented
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OOPMultipleInterface {
+
+}
---
+++ OOP/Solution/Multiple/OOPMultipleMethod.java
@@ -0,0 +1,14 @@
+package OOP.Solution.Multiple;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by ashiber on 29-May-17.
+ */
+@Documented
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OOPMultipleMethod {
+
+}
