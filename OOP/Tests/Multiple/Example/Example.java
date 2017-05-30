02c0effde25010532fb1b1091a75db40d7ec8d8c
aviadshiber <aviadshiber@campus.technion.ac.il>
2017-05-30 11:55:52 +0300

    implementing validation of multiple inheritance

--- OOP/Tests/Multiple/Example/Example.java
+++ OOP/Tests/Multiple/Example/Example.java
@@ -5,6 +5,8 @@ import OOP.Provided.Multiple.OOPMultipleException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Example {
 
     private OOPMultipleClassGenerator generator = new OOPMultipleClassGenerator();
@@ -13,6 +15,9 @@ public class Example {
     public void main() {
         try {
             I3 obj = (I3) generator.generateMultipleClass(I3.class);
+
+            Arrays.stream(I2.class.getAnnotations()).forEach(clazz-> System.out.println(clazz));
+
             Assert.assertEquals("C1 : f", obj.f());
             obj.g();
 
